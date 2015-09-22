88e9d338ae11674e11cf37d7d618fd222ea7c313
calebatwood <calebatwood@Calebs-MacBook-Air.local>
2015-09-22 13:29:16 -0600

    Begin writing exercises

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 // 1. Output each item in the following Array to your console:
 var livingRoom = ['couch', 'lamp', 'rug', 'shelf'];
-
-// 2. Given the following Array, console log 'Even' if the number is even, 'Even and greater than by 10' if the
-//    number is even and greater than 10, and 'Odd' if the number is odd.
-var numArray = [2, 17, 9, 24, 8];
  
+// 2. Given the following Array, console log 'Even' if the number is even, 'Even and greater than 10' if the
+//    number is even and greater than 10, and 'Odd' if the number is odd.
+//    HINT: Google 'remainder operator'
+var numArray = [2, 17, 9, 24, 8];
