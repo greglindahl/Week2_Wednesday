7eb234d52513073828a3afae9b930768f6bd7b90
calebatwood <calebatwood@Calebs-MacBook-Air.local>
2015-09-23 17:25:12 -0600

    Add meme

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ var livingRoom = ['couch', 'lamp', 'rug', 'shelf'];
 //     Print out the sentence "At home, I have _____ cats." Use the number from your counter to fill in the number.
 //     The numbers should range from 10 to 100, in increments of 25.
 
+
 // 6. Given the following Array, console log 'Even' if the number is even, 'Even and greater than 10' if the
 //    number is even and greater than 10, and 'Odd' if the number is odd.
 //    HINT: Google 'remainder operator'
