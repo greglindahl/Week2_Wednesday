9abfdef020dc470b4788410d14f59699efd92962
greglindahl <greglindahl410@gmail.com>
2015-09-27 12:01:22 -0600

    Finished most of problems.  Could not get 19 or 20 or get out of infinite loop on last problem

--- main.js
+++ main.js
@@ -92,8 +92,18 @@ console.log(primeArray);
 
 // 9. Prompt a user to pick either a number or a word. Create a script that alerts the user whether they chose a number or a word.
 // Hint: Google 'typeof'
+var userPick = prompt('Chose a number or a word and type it in to the box.');
+var userPickType = userPick.typeof;
+alert('You choose a ' + userPickType);
 
 // 10. Write a loop that outputs the following to the console:
+var hash = '';
+var loops = 0;
+while (loops < 7) {
+	loops++;
+	hash = hash + '#';
+	console.log(hash);
+}
 // #
 // ##
 // ###
@@ -107,11 +117,77 @@ console.log(primeArray);
 // We're going to create a paper, rock, scissors game that prompts you for your choice and allows the computer to
 // randomly choose. You will use an alert to define the winner.
 
+var human = prompt('rock, paper, scissors! Please enter your choice.');
+var choices = ['rock', 'paper', 'scissors'];
+var computer = Math.floor(Math.random() * 100);
+console.log(computer);
+if (computer <= 0 || computer <= 33) {
+	var computer = 'rock';
+	console.log(computer);
+} else if (computer <= 34 || computer <= 66) {
+	var computer = 'paper';
+	console.log(computer);
+} else if (computer <= 67 || computer <= 100) {
+	var computer = 'scissors';
+	console.log(computer);
+}
+
+if (human === 'rock') {
+	if (computer === 'paper') {
+		console.log('paper wins');
+	} else if (computer === 'scissors') {
+		console.log('rock wins');
+	} else if (human === 'paper') {
+		if (computer === 'rock') {
+			cosole.log('paper wins');
+		} else if (computer === 'scissors') {
+			console.log('scissors win');
+		} else if (human === 'scissors') {
+			if (computer === 'rock') {
+				console.log('rock wins');
+			} else if (computer === 'paper') {
+				console.log('scissors wins');
+			}
+		}
+	}
+}
+
+// if (human != 'rock' || human != 'paper' || human != 'scissors') {
+// 	alert('Please enter rock paper or scissors');
+// 	var human = prompt('rock, paper, scissors! Please enter your choice.');
+// } else if (human === 'rock') {
+// 	if (computer === 'paper') {
+// 		console.log('paper wins');
+// 	} else if (computer === 'scissors') {
+// 		console.log('rock wins');
+// 	} else if (human === 'paper') {
+// 		if (computer === 'rock') {
+// 			cosole.log('paper wins');
+// 		} else if (computer === 'scissors') {
+// 			console.log('scissors win');
+// 		} else if (human === 'scissors') {
+// 			if (computer === 'rock') {
+// 				console.log('rock wins');
+// 			} else if (computer === 'paper') {
+// 				console.log('scissors wins');
+// 			}
+// 		}
+// 	}
+// }
+
+
+
 // 11. Use a variable called "human" to prompt the user to type their choice.
+// var human = prompt('Rock, Paper, Scissors! Please enter your choice.');
+
 // 12. Define an array called "choices" that consists of "paper", "rock", and "scissors".
+// var choices = ['rock', 'paper', 'scissors'];
+
 // 13. Define a variable called "computer" and use Math.random to allow the computer to randomly select a number.
 //    For reference:
 //    Math.random gives you a random number between 0 and 1, which is different each time you call it.
+// var computer = Math.random() * 100;
+
 // 14. Let's start our conditional statement. Start by reassigning the computer variable to "rock" if the random number
 //    falls between 0 and .33. Run the code until the console prints out "rock" instead of the number.
 //    In English, it will look like this:
@@ -119,6 +195,7 @@ console.log(primeArray);
 //       if the number is between 0 and 33
 //          computer = "rock"
 //       log computer to the console
+
 // 15. Now add the following conditionals to the same statement:
 //    If the random number is between .34 and .66, set the computer variable to "paper".
 //    If the random number is between .67 and 1, set the computer variable to "scissors". (Who the hell seriously chooses scissors first?)
@@ -140,18 +217,36 @@ console.log(primeArray);
 
 // HEDS ER TALES?
 // 21. Use the following variable for your coin flip action:
-       var coin = Math.floor(Math.random() * 2);
+       
 //     We're using Math.random again, along with Math.floor.
 //     Remember, Math.random gives you a random number between 0 and 1.
 //     Calling Math.floor on that number will truncate the decimal, and give you a
 //     random number within the bounds of your array. (In this case, our array will only contain two items.)
 // 22. Use a while loop to keep flipping the coin until you get tails.
+var coin = Math.floor(Math.random() * 2);
+while (coin === 0) {
+	console.log('Heads. Flip again');
+	var coin = Math.floor(Math.random() * 2);
+}
+console.log(coin);
+console.log('Tails. You got it!');
 
 // 23. FIZZ BUZZ
 // Write a program that uses console.log to print each number up to 100, with a couple exceptions...
 // If the number is divisible by 3, print "Fizz" instead of the number.
 // If the number is divisible by 5, print "Buzz" instead of the number.
 // If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
+for (fbNum = 1; fbNum <= 100; fbNum++) {
+	if (fbNum % 3 == 0 && fbNum % 5 == 0) {
+		console.log("FizzBuzz");
+	} else if (fbNum % 5 == 0) {
+		console.log("Buzz");
+	} else if (fbNum % 3 == 0) {
+		console.log("Fizz");
+	} else {
+		console.log(fbNum);
+	}
+}
 
 
 // CHESS BOARD
@@ -168,21 +263,71 @@ console.log(primeArray);
 // # # # #
 //  # # # #
 // # # # #
+console.log('Question 24a');
+var size = 8;
+var board = '';
+
+for (var y = 0; y < size; y++) { 
+for (var x = 0; x < size; x++) {
+if ((x + y) % 2 == 0) 
+board += ' ';
+else
+board += '#';
+}
+board += '\n';
+}
+
+console.log(board);
 
 // When you have a program that generates this pattern, define a variable size = 8 and change the program
 // sso that it works for any size, outputting a grid of the given width and height.
+console.log('Question 24b');
+var size = prompt("Enter a number from 1 - 100");
+var board = '';
+
+for (var y = 0; y < size; y++) { 
+for (var x = 0; x < size; x++) {
+if ((x + y) % 2 == 0) 
+board += ' ';
+else
+board += '#';
+}
+board += '\n';
+}
+
+console.log(board);
 
 
 // THAR BE DACHSHUNDS
 // We're going to slay some badass dachshunds.
 // 25. Start by using the following variables:
+
+// 26. Create a while loop that runs as long as "slaying" is equal to true. For now, set "slaying" to false
+//     inside of the body of the loop so we don't break stuff with an infinite loop.
 var slaying = true;
 var youHit = Math.floor(Math.random() * 2);
 var damageThisRound = Math.floor(Math.random()*5 + 1);
 var totalDamage = 0;
 
-// 26. Create a while loop that runs as long as "slaying" is equal to true. For now, set "slaying" to false
-//     inside of the body of the loop so we don't break stuff with an infinite loop.
+
+while (slaying = true) {
+	slaying = false;
+	if (youHit == 1) {
+		youHit = Math.floor(Math.random() * 2);
+		console.log('Great! You hit the dog');
+		totalDamage = totalDamage + damageThisRound;
+		if (totalDamage == 4) {
+			console.log('You killed the Dog!');
+			slaying = true;
+		} else if (totalDamage < 4) {
+			youHit = Math.floor(Math.random() * 2);
+		}
+	} else if (youHit == 0) {
+		youHit = Math.floor(Math.random() * 2);
+		console.log('Ugh, the dog is kicking your butt!');
+	}
+}
+
 // 27. Inside of your while loop, create a conditional statement. If "youHit" is 1 (which is true),
 //     log to the console a congratulatory message about injuring the poor dog.
 //     If "youHit" is 0 (which is false), log to the console a message about that bastard beating you.
